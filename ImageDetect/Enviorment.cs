8f7d5c1a56b0f7e187f7d72266ebcda0cda9d306
alexsilvar <alexskysme@gmail.com>
2017-12-11 14:08:32 -0200

    Enviorment and Strategy codes

---
+++ ImageDetect/Enviorment.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageDetect
+{
+    class Environment
+    {
+        private Point _currentBall, _lastBall, _predictBall;//Bolas e predicts
+        private Rectangle _fieldBounds;
+        private Rectangle _goalBounds;
+        private Rectangle _opponetGoalBounds;//Areas onde os robôs devem ficar e fazer gol
+
+
+        public Environment(Rectangle field, Rectangle goal, Rectangle opponentGoal)
+        {
+            FieldBounds = field;
+            GoalBounds = goal;
+            OpponetGoalBounds = opponentGoal;
+
+            _currentBall = Point.Empty;
+            _lastBall = Point.Empty;
+        }
+
+        public Rectangle FieldBounds
+        {
+            get
+            {
+                return _fieldBounds;
+            }
+
+            set
+            {
+                _fieldBounds = value;
+            }
+        }
+
+        public Rectangle GoalBounds
+        {
+            get
+            {
+                return _goalBounds;
+            }
+
+            set
+            {
+                _goalBounds = value;
+            }
+        }
+
+        public Rectangle OpponetGoalBounds
+        {
+            get
+            {
+                return _opponetGoalBounds;
+            }
+
+            set
+            {
+                _opponetGoalBounds = value;
+            }
+        }
+
+        public Point predictBall()
+        {
+            //Predict simples, pega a ultima posição e testa a variação em X e Y, para imaginar o próximo estado
+            int dx = _currentBall.X - _lastBall.X;
+            int dy = _currentBall.Y - _lastBall.Y;
+            _predictBall = new Point(_currentBall.X + dx, _currentBall.Y + dy);
+            return _predictBall;
+        }
+        public Point currentBall()
+        {
+            return _currentBall;
+        }
+        public void newBall(Point ball)
+        {
+            _lastBall = _currentBall;
+            _currentBall = ball;
+        }
+    }
+}
---
+++ ImageDetect/Strategy.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageDetect
+{
+    class Strategy
+    {
+        //Posicao 0 goleiro, 1 zagueiro, 2 atacante
+        private Robo[] robot;
+        private Point ball;
+        private int gameState;
+        private Environment env;
+
+        public Robo[] Robot
+        {
+            get
+            {
+                return robot;
+            }
+
+            set
+            {
+                robot = value;
+            }
+        }
+
+        public Strategy(int gameState, Environment envio)
+        {
+            this.gameState = gameState;
+            env = envio;
+        }
+
+
+
+        public void step()
+        {
+            atacante();
+            zagueiro();
+            goleiro();
+        }
+
+        private void zagueiro()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void atacante()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void goleiro()
+        {
+            double velocityLeft = 0, velocityRight = 0;
+
+            double Tx = env.GoalBounds.Right - env.currentBall().X;
+            double Ty = env.FieldBounds.Top - env.currentBall().Y;
+
+            double Ax = env.GoalBounds.Right - Robot[0].Position.X;
+            double Ay = env.FieldBounds.Top - Robot[0].Position.Y;
+        }
+    }
+}
